c3aeb0d5c456bb482421d4025ab48475965fb72d
CARTOON01 <brianngugi224@gmail.com>
2023-09-08 00:31:21 +0300

    update on task 4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
-"""
-This fabric script generates a .tgz archive from the contents of the web_static
-folder of your AirBnB Clone repo, using the function do_pack
-"""
-from fabric.api import local
-from datetime import datetime
+# Fabfile to delete out-of-date archives.
+import os
+from fabric.api import *
+
+env.hosts = ["18.234.107.37", "52.91.151.209"]
 
 
-def do_pack():
+def do_clean(number=0):
+    """Delete out-of-date archives.
+
+    Args:
+        number (int): The number of archives to keep.
+
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
     """
-    Creates a .tgz archive
-    """
-    time = datetime.now().strftime('%Y%m%d%H%M%S')
-    path = "versions/web_static_{}.tgz".format(time)
-    local("mkdir -p versions")
+    number = 1 if int(number) == 0 else int(number)
 
-    if local("tar -cvzf {} web_static".format(path)).succeeded:
-        return path
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
